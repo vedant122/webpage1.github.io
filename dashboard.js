ff0c47b9d9ced92c343b3aca89a64add7ae5b982
vedant122 <84017615+vedant122@users.noreply.github.com>
2022-08-28 13:10:02 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+function expand(){
+    var c=document.getElementById("d")
+    document.getElementById("add").style.display="none"
+    document.getElementById("min").style.display="inline"
+    c.style.display="inline"
+}
+
+function min(){
+    var c=document.getElementById("d")
+    document.getElementById("min").style.display="none"
+    document.getElementById("add").style.display="inline"
+    c.style.display="none"
+}
